a81f66684b0e9b7002eb3d3d4133c163c3ee9f05
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-30 23:53:26 -0700

    database created to handle the text data

---
+++ client/src/js/database.js
@@ -0,0 +1,47 @@
+import { openDB } from 'idb';
+
+const DB_NAME = 'WordWaveDB';
+const STORE_NAME = 'EditorContent';
+
+const initdb = async () => {
+  openDB(DB_NAME, 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+        console.log(`${STORE_NAME} database created`);
+      } else {
+        console.log(`${STORE_NAME} database already exists`);
+      }
+    },
+  });
+};
+
+export const saveText = async (content) => {
+  try {
+    const timestamp = new Date().toISOString();
+    const db = await openDB(DB_NAME, 1);
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    const result = await store.add({ content, timestamp });
+    console.log('Content saved to the database', result);
+  } catch (err) {
+    console.error("Error saving content to the database:", err);
+  }
+};
+
+export const getLastSavedText = async (callback) => {
+  try {
+    const db = await openDB(DB_NAME, 1);
+    const tx = db.transaction(STORE_NAME, 'readonly');
+    const store = tx.objectStore(STORE_NAME);
+    const result = await store.getAll();
+    if (result.length > 0) {
+      const lastSavedContent = result[result.length - 1].content;
+      callback(lastSavedContent);
+    }
+  } catch (err) {
+    console.error("Error retrieving content from the database:", err);
+  }
+};
+
+initdb();
