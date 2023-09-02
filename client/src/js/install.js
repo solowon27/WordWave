e530a5d58f7c8d33c85c1c67b39710c110409b49
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-02 10:04:47 -0700

    event listener added for install image on mobile view

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,32 +1,39 @@
 const butInstall = document.getElementById("buttonInstall");
+const installImage = document.getElementById("installImage");
 
-window.addEventListener('beforeinstallprompt', (event) => {
-
-    // Store the triggered events
-    window.deferredPrompt = event;
-
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
-  });
-
-butInstall.addEventListener('click', async () => {
-  
+// Function to show the install prompt
+const showInstallPrompt = () => {
   const promptEvent = window.deferredPrompt;
+  if (promptEvent) {
+    // Show prompt
+    promptEvent.prompt();
 
-  if (!promptEvent) {
-   return;
+    // Reset the deferred prompt variable, it can only be used once
+    window.deferredPrompt = null;
+
+    // Hide the install button and image after the prompt is shown
+    butInstall.style.display = 'none';
+    installImage.style.display = 'none';
   }
+};
 
-  // Show prompt
-  promptEvent.prompt();
-  
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
+butInstall.addEventListener("click", showInstallPrompt);
+installImage.addEventListener("click", showInstallPrompt);
+
+// Event listener to detect the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Store the triggered event
+  window.deferredPrompt = event;
+
+  // Hide the default install button initially
+  butInstall.style.display = 'none';
+
+  // Show the install image
+  installImage.style.display = 'inline-block';
 });
 
+// Event listener to detect the appinstalled event
 window.addEventListener('appinstalled', (event) => {
   // Clear prompt
   window.deferredPrompt = null;
-}); 
+});
